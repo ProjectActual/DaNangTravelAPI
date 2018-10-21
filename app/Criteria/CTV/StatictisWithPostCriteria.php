d8886be7b406f983223714773a549cbdd5cccbde
tran truong quy <trantruongquy2702@gmail.com>
2018-10-21 15:49:42 +0700

    feat(Api):statistic 30%

--- app/Criteria/CTV/StatictisWithPostCriteria.php
+++ app/Criteria/CTV/StatictisWithPostCriteria.php
@@ -5,6 +5,8 @@ namespace App\Criteria\CTV;
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Contracts\RepositoryInterface;
 
+use App\Entities\Role;
+
 /**
  * Class StatictisWithPostCriteria.
  *
@@ -31,6 +33,8 @@ class StatictisWithPostCriteria implements CriteriaInterface
         //Get posts by user and filter by month and year
         return $model
             ->selectRaw('users.*, count(posts.id) as count_posts, sum(posts.count_view) as viewer_interactive')
+            ->join('role_user', 'users.id', '=', 'role_user.user_id')
+            ->where('role_user.role_id', Role::CODE_NAME['CONGTACVIEN'])
             ->join('posts', 'posts.user_id', '=', 'users.id')
             ->whereMonth('posts.created_at', '=', $this->date->month)
             ->whereYear('posts.created_at', '=', $this->date->year)
