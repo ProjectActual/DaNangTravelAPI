50230944f797d11c1d02cc494f7b6be36344cb69
tran truong quy <trantruongquy2702@gmail.com>
2018-11-07 10:47:22 +0700

    fix exception

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'NEXMO' => [
+        'SMS' => [
+            'KEY'    => env('NEXMO_API_KEY', '5f133be7'),
+            'SECRET' => env('NEXMO_API_SECRET', '8pULXfpAJEgiI1ow'),
+            'FROM'   => env('NEXMO_FROM_SMS', '0522796213'),
+        ],
+    ],
+];
+?>
